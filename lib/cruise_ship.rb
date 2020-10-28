fa71c458b7b44ad1d7acb7594f6f464645f7de41
Kishana Liburd <kishana.liburd@gmail.com>
2020-10-28 14:21:58 +0000

    Done.

--- lib/cruise_ship.rb
+++ lib/cruise_ship.rb
@@ -8,4 +8,11 @@
 
 def select_winner(passengers)
   # add the code snippet here!
+  winner = " "
+  passengers.each do |suites, name|
+    if suites == :suite_a && name.start_with?("A")
+      winner = name
+end
+end
+winner
 end
